79ca5a68e99bf2ff94243fc7702955f6ffc4912c
Geoffrey Lee <glee@pculture.org>
2011-01-11 22:58:02 +0900

    Whoups.  Fixup the filebackend.

--- filebackend.py
+++ filebackend.py
@@ -59,14 +59,14 @@ class Backend(object):
             path = os.path.join(self.path, entry)
             if os.path.isfile(os.path.join(self.path, entry)):
                 item = dict()
-                item['dmap.itemkind'] = c
+                item['dmap.itemid'] = c
                 item['dmap.itemname'] = nam
                 item['daap.songformat'] = ext
                 media_kind = None
                 if ext in self.audio_ext:
-                    media_kind = ('aeMK', libdaap.DAAP_MEDIAKIND_AUDIO)
+                    media_kind = libdaap.DAAP_MEDIAKIND_AUDIO
                 elif ext in self.video_ext:
-                    media_kind = ('aeMK', libdaap.DAAP_MEDIAKIND_VIDEO)
+                    media_kind = libdaap.DAAP_MEDIAKIND_VIDEO
                 if media_kind:
                     item['com.apple.itunes.mediakind'] = media_kind
                     self.items[c] = item
